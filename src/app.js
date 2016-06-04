dc5383e054eb28349088d38dc93bbc1d5758ec31
Steven Occhipinti <steve.occhipinti@rea-group.com>
2016-06-04 14:18:05 +1000

    Basic usage of state and props

--- src/app.js
+++ src/app.js
@@ -2,10 +2,40 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import { Router, Route, Link, browserHistory } from 'react-router'
 
-const Hello = React.createClass({
+const App = React.createClass({
+  getInitialState() {
+    return {
+      users: ["Bob", "Fred"]
+    };
+  },
+
+  users() {
+    return this.state.users.map((user, i) => {
+      return <Person key={i} name={user} />;
+    });
+  },
+
+  addUser() {
+    var users = this.state.users;
+    users.push("Larry");
+    this.setState({users: users});
+  },
+
   render() {
     return (
-      <h2>bla</h2>
+      <div>
+        <ul>{this.users()}</ul>
+        <button type="button" onClick={this.addUser}>Add</button>
+      </div>
+    );
+  }
+});
+
+
+const Person = React.createClass({
+  render() {
+    return (
+      <li>{this.props.name}</li>
     );
   }
 });
@@ -13,4 +43,4 @@ const Hello = React.createClass({
 // Declarative route configuration (could also load this config lazily
 // instead, all you really need is a single root route, you don't need to
 // colocate the entire config).
-ReactDOM.render(<Hello />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
